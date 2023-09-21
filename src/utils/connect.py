43ed3333371df28acb43ca06cf14e270612663b1
SpinyKarma <samuel.shaw52@gmail.com>
2023-09-21 16:28:16 +0100

    move dict_res to connect.py and rename to run

--- src/utils/connect.py
+++ src/utils/connect.py
@@ -14,3 +14,25 @@ def connect():
         .env file.
     '''
     return Connection(user, database="apiknights", password=password)
+
+
+def run(db, query):
+    ''' Runs a query to a postgres database and returns the response as a list
+        of dictionaies with the column headings as keys and the row data as
+        values.
+
+        Args:
+            db:
+                The connection object to run the query string with.
+            query:
+                The query string to be run.
+
+        Returns:
+            res_dicts:
+                The response from the db formatted as a list of dicts, one per
+                returned row, with the column headings as the keys.
+    '''
+    res = db.run(query)
+    cols = [col["name"] for col in db.columns]
+    res_dicts = [{cols[i]: item[i] for i in range(len(cols))} for item in res]
+    return res_dicts
