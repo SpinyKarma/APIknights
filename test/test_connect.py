e65432852785f552b8fade87df5516ba8b47d94e
SpinyKarma <samuel.shaw52@gmail.com>
2023-09-25 12:22:03 +0100

    name standardisation

--- test/test_connect.py
+++ test/test_connect.py
@@ -5,10 +5,10 @@ from unittest.mock import Mock, patch
 class Test_connect:
     @patch("src.utils.connect.getenv")
     @patch("src.utils.connect.Connection")
-    def test_passes_env_vars_to_connection_object(self, mock_Con, mock_env):
-        mock_env.side_effect = ["lemon", "orange", "banana"]
+    def test_passes_env_vars_to_connection_object(self, m_Con, m_env):
+        m_env.side_effect = ["lemon", "orange", "banana"]
         connect()
-        mock_Con.assert_called_with(
+        m_Con.assert_called_with(
             "lemon",
             database="orange",
             password="banana"
@@ -17,52 +17,52 @@ class Test_connect:
 
 class Test_run:
     @patch("src.utils.connect.connect")
-    def test_queries_db_with_passed_query(self, mock_connect):
-        mock_db = Mock()
-        mock_db.run.return_value = []
-        mock_db.columns = []
-        mock_connect.return_value.__enter__.return_value = mock_db
+    def test_queries_db_with_passed_query(self, m_connect):
+        m_db = Mock()
+        m_db.run.return_value = []
+        m_db.columns = []
+        m_connect.return_value.__enter__.return_value = m_db
         run("banana")
-        mock_db.run.assert_called_with("banana")
+        m_db.run.assert_called_with("banana")
 
     @patch("src.utils.connect.connect")
-    def test_returns_empty_list_when_db_responds_empty(self, mock_connect):
-        mock_db = Mock()
-        mock_db.run.return_value = []
-        mock_db.columns = []
-        mock_connect.return_value.__enter__.return_value = mock_db
+    def test_returns_empty_list_when_db_responds_empty(self, m_connect):
+        m_db = Mock()
+        m_db.run.return_value = []
+        m_db.columns = []
+        m_connect.return_value.__enter__.return_value = m_db
         assert run("") == []
 
     @patch("src.utils.connect.connect")
-    def test_one_dict_in_list_when_db_one_row_response(self, mock_connect):
-        mock_db = Mock()
-        mock_db.run.return_value = [["banana"]]
-        mock_db.columns = [{"name": "fruit"}]
-        mock_connect.return_value.__enter__.return_value = mock_db
+    def test_one_dict_in_list_when_db_one_row_response(self, m_connect):
+        m_db = Mock()
+        m_db.run.return_value = [["banana"]]
+        m_db.columns = [{"name": "fruit"}]
+        m_connect.return_value.__enter__.return_value = m_db
         assert run("") == [{"fruit": "banana"}]
 
     @patch("src.utils.connect.connect")
-    def test_one_dict_per_row_when_db_many_row_response(self, mock_connect):
-        mock_db = Mock()
-        mock_db.run.return_value = [["banana"], ["lemon"]]
-        mock_db.columns = [{"name": "fruit"}]
-        mock_connect.return_value.__enter__.return_value = mock_db
+    def test_one_dict_per_row_when_db_many_row_response(self, m_connect):
+        m_db = Mock()
+        m_db.run.return_value = [["banana"], ["lemon"]]
+        m_db.columns = [{"name": "fruit"}]
+        m_connect.return_value.__enter__.return_value = m_db
         assert run("") == [{"fruit": "banana"}, {"fruit": "lemon"}]
 
     @patch("src.utils.connect.connect")
-    def test_one_key_per_col_when_db_many_col_response(self, mock_connect):
-        mock_db = Mock()
-        mock_db.run.return_value = [["banana", "yellow"]]
-        mock_db.columns = [{"name": "fruit"}, {"name": "colour"}]
-        mock_connect.return_value.__enter__.return_value = mock_db
+    def test_one_key_per_col_when_db_many_col_response(self, m_connect):
+        m_db = Mock()
+        m_db.run.return_value = [["banana", "yellow"]]
+        m_db.columns = [{"name": "fruit"}, {"name": "colour"}]
+        m_connect.return_value.__enter__.return_value = m_db
         assert run("") == [{"fruit": "banana", "colour": "yellow"}]
 
     @patch("src.utils.connect.connect")
-    def test_many_col_many_row_res(self, mock_connect):
-        mock_db = Mock()
-        mock_db.run.return_value = [["banana", "yellow"], ["orange", "orange"]]
-        mock_db.columns = [{"name": "fruit"}, {"name": "colour"}]
-        mock_connect.return_value.__enter__.return_value = mock_db
+    def test_many_col_many_row_res(self, m_connect):
+        m_db = Mock()
+        m_db.run.return_value = [["banana", "yellow"], ["orange", "orange"]]
+        m_db.columns = [{"name": "fruit"}, {"name": "colour"}]
+        m_connect.return_value.__enter__.return_value = m_db
         assert run("") == [
             {"fruit": "banana", "colour": "yellow"},
             {"fruit": "orange", "colour": "orange"}
